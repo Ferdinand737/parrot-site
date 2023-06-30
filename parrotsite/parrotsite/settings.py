e3e7023956cbd8ed0a26f8beca48eb854757ad89
https://github.com/Caipo <mr.dino.rex@gmail.com>
2023-06-30 12:33:45 -0700

    Added user auth for discord

--- parrotsite/parrotsite/settings.py
+++ parrotsite/parrotsite/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),  
-        'PORT': '3306',  
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
