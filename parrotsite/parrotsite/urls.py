23e69b22440325ba5b6c6666b46a0c3e9af694aa
Ferdinand <ferdinandhaaben@gmail.com>
2023-07-03 08:07:27 -0700

    started on shop page

--- parrotsite/parrotsite/urls.py
+++ parrotsite/parrotsite/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('discordlogin.urls')),
+    path('shop/', include('payments.urls'))
 ]
